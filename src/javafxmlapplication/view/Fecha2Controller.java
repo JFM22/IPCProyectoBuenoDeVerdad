d7df487c489cdac2a183160ecb532c84277c6fc0
Isbuz <isaaccb2003@gmail.com>
2023-05-28 19:46:54 +0200

    edrsdf

--- src/javafxmlapplication/view/Fecha2Controller.java
+++ src/javafxmlapplication/view/Fecha2Controller.java
@@ -4,6 +4,7 @@
  */
 package javafxmlapplication.view;
 
+import java.io.IOException;
 import java.net.URL;
 import java.time.DayOfWeek;
 import java.time.Duration;
@@ -18,6 +19,9 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Optional;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleBooleanProperty;
@@ -38,6 +42,8 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import model.Booking;
+import model.Club;
+import model.ClubDAOException;
 import model.Court;
 import model.Member;
 import utils.Usuario;
@@ -66,6 +72,10 @@ public class Fecha2Controller implements Initializable {
     LocalDateTime fechaHoy;
     LocalTime horaInicio;
     boolean paid=false;
+    boolean alertaAceptar=false;
+    boolean esLibre=false;
+    private Club club;
+    List<Booking> reservas;
     //es del ejemplo
      private final LocalTime firstSlotStart = LocalTime.of(9, 0);
     private final Duration slotLength = Duration.ofMinutes(60);
@@ -75,13 +85,42 @@ public class Fecha2Controller implements Initializable {
     private static final PseudoClass SELECTED_PSEUDO_CLASS = PseudoClass.getPseudoClass("selected");
 
     private List<TimeSlot> timeSlots = new ArrayList<>(); //Para varias columnas List<List<TimeSolt>>
-
+    
     private ObjectProperty<TimeSlot> timeSlotSelected;
     
     private LocalDate daySelected;
+    
     @FXML
     private Label slotSelected;
-    
+    TimeSlot aux;
+    @FXML
+    private Label label1;
+    @FXML
+    private Label label2;
+    @FXML
+    private Label label3;
+    @FXML
+    private Label label4;
+    @FXML
+    private Label label5;
+    @FXML
+    private Label label6;
+    @FXML
+    private Label label7;
+    @FXML
+    private Label label8;
+    @FXML
+    private Label label9;
+    @FXML
+    private Label label10;
+    @FXML
+    private Label label11;
+    @FXML
+    private Label label12;
+    @FXML
+    private Label label13;
+    private Label []etiquetas;
+    int LabelIndex;
     /**
      * Initializes the controller class.
      */
@@ -89,14 +128,36 @@ public class Fecha2Controller implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         Combo.getItems().addAll("Pista 1","Pista 2", "Pista 3","Pista 4", "Pista 5", "Pista 6");
         Combo.setValue("Pista 1");
+        etiquetas= new Label[]{label1,label2,label3,label4,label5,label6,label7,label8,label9,label10,label11,label12,label13};
+        pista.setName(Combo.getValue());
+        //versionFinal-------
+        //user = Usuario.getInstancia();
+       //member = user.getUsuario();
+        //pruebas---------
+        try {
+            
+            club = Club.getInstance();
+        } catch (ClubDAOException ex) {
+            Logger.getLogger(Fecha2Controller.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Fecha2Controller.class.getName()).log(Level.SEVERE, null, ex);
+        }
         timeSlotSelected = new SimpleObjectProperty<>();
-
+        user = Usuario.getInstancia();
+         member = club.getMemberByCredentials("vege", "7777777");
+          user.setUsuario(member);
         //---------------------------------------------------------------------
         //cambia los SlotTime al cambiar de dia
         dpBooking.valueProperty().addListener((a, b, c) -> {
-            setTimeSlotsGrid(c);
+            setTimeSlotsGrid(c,pista);
             labelCol.setText(c.getDayOfWeek().getDisplayName(TextStyle.FULL_STANDALONE, Locale.getDefault()));
         });
+         Combo.valueProperty().addListener((a, b, c) -> {
+             pista.setName(c);
+            setTimeSlotsGrid(dpBooking.getValue(),pista);
+            
+        });
+         
         
         //---------------------------------------------------------------------
         //inicializa el DatePicker al dia actual
@@ -104,8 +165,8 @@ public class Fecha2Controller implements Initializable {
 
         //---------------------------------------------------------------------
         // pinta los SlotTime en el grid
-        setTimeSlotsGrid(dpBooking.getValue());
-
+        setTimeSlotsGrid(dpBooking.getValue(),pista);
+        
       
         //---------------------------------------------------------------------
         // enlazamos timeSlotSelected con el label para mostrar la seleccion
@@ -123,75 +184,108 @@ public class Fecha2Controller implements Initializable {
         
         
     }
- private void setTimeSlotsGrid(LocalDate date) {
+ private void setTimeSlotsGrid(LocalDate date,Court pista) {
         //actualizamos la seleccion
         timeSlotSelected.setValue(null);
-
+       reservas = club.getCourtBookings(pista.getName(),  date);
         //--------------------------------------------        
         //borramos los SlotTime del grid
-        ObservableList<Node> children = grid.getChildren();
+        ObservableList<Node> children = grid.getChildren();//te lo guarda todo en una ObservableList (los guarda como Node)
         for (TimeSlot timeSlot : timeSlots) {
-            children.remove(timeSlot.getView());
+           children.remove(timeSlot.getView());
         }
         timeSlots = new ArrayList<>();
-
         //----------------------------------------------------------------------------------
         // desde la hora de inicio y hasta la hora de fin creamos slotTime segun la duracion
         int slotIndex = 1;
-        for (LocalDateTime startTime = date.atTime(firstSlotStart);
-                !startTime.isAfter(date.atTime(lastSlotStart));
-                startTime = startTime.plus(slotLength)) {
+        LabelIndex=0;
+        for (LocalDateTime startTime = date.atTime(firstSlotStart);!startTime.isAfter(date.atTime(lastSlotStart));startTime = startTime.plus(slotLength)) {
 
             //---------------------------------------------------------------------------------------
             // creamos el SlotTime, lo anyadimos a la lista de la columna y asignamos sus manejadores
-            TimeSlot timeSlot = new TimeSlot(startTime, slotLength);
+            for(Booking aux : reservas){
+             //LocalDateTime bookingEndTime = aux.getBookingDate().plusMinutes(120);
+             aux.getFromTime();
+             if(aux.getBookingDate().equals(startTime)){
+                   esLibre=false;
+                    break;
+            } 
+                else {
+                    esLibre=true;
+                }
+            }
+            
+            
+            TimeSlot timeSlot = new TimeSlot(startTime, slotLength,etiquetas[LabelIndex],esLibre);
+            
             timeSlots.add(timeSlot);
-            registerHandlers(timeSlot);
+            registerHandlers(timeSlot,esLibre,LabelIndex);
             //-----------------------------------------------------------
             // lo anyadimos al grid en la posicion x= 1, y= slotIndex
+            
             grid.add(timeSlot.getView(), 1, slotIndex);
+           
+            LabelIndex++;
             slotIndex++;
         }
+        
+        
     }
- private void registerHandlers(TimeSlot timeSlot) {
-
+ private void registerHandlers(TimeSlot timeSlot,boolean esLibre,int numLabel) {
+        
         timeSlot.getView().setOnMousePressed((MouseEvent event) -> {
             //---------------------------------------------slot----------------------------
             //solamente puede estar seleccionado un slot dentro de la lista de slot
             timeSlots.forEach(slot -> {
                 slot.setSelected(slot == timeSlot);
+                aux=timeSlot;
+                               
+
             });
 
             //----------------------------------------------------------------
             //actualizamos el label Dia-Hora, esto es ad hoc,  para mi diseño
             timeSlotSelected.setValue(timeSlot);
-            //----------------------------------------------------------------
-            // si es un doubleClik  vamos a mostrar una alerta y cambiar el estilo de la celda
-            if (event.getClickCount() > 1) {
-                Alert alerta = new Alert(Alert.AlertType.CONFIRMATION);
-                alerta.setTitle("SlotTime");
-                alerta.setHeaderText("Confirma la selecció");
-                alerta.setContentText("Has seleccionat: "
-                        + timeSlot.getDate().format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT)) + ", "
-                        + timeSlot.getTime().format(DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT)));
-                Optional<ButtonType> result = alerta.showAndWait();
-                if (result.isPresent() && result.get() == ButtonType.OK) {
-                    ObservableList<String> styles = timeSlot.getView().getStyleClass();
-                    if (styles.contains("time-slot")) {
-                        styles.remove("time-slot");
-                        styles.add("time-slot-libre");
-                    } else {
-                        styles.remove("time-slot-libre");
-                        styles.add("time-slot");
-                    }
-                }
-            }
+        
+       
         });
+        
     }
 
     @FXML
     private void ReservarClicked(ActionEvent event) {
-    
+        
+                Alert alerta = new Alert(Alert.AlertType.CONFIRMATION);
+                alerta.setTitle("SlotTime");
+                alerta.setHeaderText("Confirma la selección");
+                alerta.setContentText("Has seleccionado: "
+                        + aux.getDate().format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT)) + ", "
+                        + aux.getTime().format(DateTimeFormatter.ofLocalizedTime(FormatStyle.SHORT)));//fin de mensaje
+                Optional<ButtonType> result = alerta.showAndWait();
+                if (result.isPresent() && result.get() == ButtonType.OK) {
+                    ObservableList<String> styles = aux.getView().getStyleClass();
+                    if (styles.contains("time-slot")) {//que si que esta disponible
+                        styles.remove("time-slot");
+                        styles.add("time-slot-libre");
+                //-------------------------------------------------------------------------------------
+                LocalDate fechaSeleccionada =dpBooking.getValue();
+                horaInicio=aux.getTime();
+                pista.setName(Combo.getValue());
+                try{
+                
+                club.registerBooking(LocalDateTime.now(),fechaSeleccionada,horaInicio,paid,pista,member);  
+                System.out.println("libre y registrada crack");
+
+                }catch(ClubDAOException e){
+                System.out.println("esta reservado maquina");
+                
+                    }
+                    } else {//ya esta pillada
+                        System.out.println("reservada");
+                        styles.remove("time-slot-libre");
+                        styles.add("time-slot");
+                    }
+                }
     }
      public class TimeSlot {
 
@@ -213,15 +307,25 @@ public class Fecha2Controller implements Initializable {
             selectedProperty().set(selected);
         }
 
-        public TimeSlot(LocalDateTime start, Duration duration) {
+        public TimeSlot(LocalDateTime start, Duration duration,Label label,boolean esLibre) {
             this.start = start;
+
             this.duration = duration;
+            
             view = new Pane();
             view.getStyleClass().add("time-slot");
             // ---------------------------------------------------------------
             // de esta manera cambiamos la apariencia del TimeSlot cuando los seleccionamos
             selectedProperty().addListener((obs, wasSelected, isSelected)
                     -> view.pseudoClassStateChanged(SELECTED_PSEUDO_CLASS, isSelected));
+            if(esLibre){
+                label.setText("Libre");
+                System.out.println("AAAAAAAAAA");
+            }else{
+                label.setText("reservada");
+                System.out.println("OOOOOOOO");
+
+            }
 
         }
 
